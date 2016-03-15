a6b75366480781de916771f2adf690ef2be703f2
hatterkiller <michaelshinoda287@gmail.com>
2016-03-15 22:03:47 +0700

    Added player stuff

--- shoutbox/ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-// File: ajax.php
-    // Ajax refresh hits
-    if(isset($_GET['refresh'])){
-        // Uses the get_page_views() function which takes an ID and retreives that page view count. The ID is passed by the AJAX script.
-        if($_GET['refresh'] == 'hits') echo get_page_views($_GET['id']);
-    };
-?>
--- shoutbox/join.php
+++ shoutbox/join.php
@@ -1,19 +1,37 @@
 <?php
 if($_POST)
 {
-$name=$_POST['name'];
-$username=$_POST['username'];
-$password=$_POST['password'];
-$gender=$_POST['gender'];
-/*mysql_query("");*/
+    /*Gets individual input values from the dataString in `editor.html`*/
+    $numberNa1=$_POST['numberNa1'];
+    $numbern1=$_POST['numbern1'];
+    $numberoi1=$_POST['numberoi1'];
+    $numbers1=$_POST['numbers1'];
+    $numberf1=$_POST['numberf1'];
     
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "Mickey Mouse\n";
-fwrite($myfile, $txt);
-$txt = "Minnie Mouse\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+    /*mysql_query("");*/
+    
+    $myfile = fopen("newfilse.txt", "w") or die("Unable to open file!"); /*Opens file and edits the file. */
+    /*Left Player 1 Data*/
+    fwrite($myfile, $numbern1. "\r\n");/*Number*/
+    fwrite($myfile, $numberNa1. "\r\n");/*Name*/
+    fwrite($myfile, $numberf1. "\r\n");/*Foul*/
+    fwrite($myfile, $numbers1. "\r\n");/*Score*/
+    fwrite($myfile, $numberoi1. "\r\n");/*O/I*/
+    
+    
+    /*Left Player 2 Data*/
+    /*Etc?*/
+    
+    
+    fclose($myfile);
     
 }else { }
+
+
+
+
+
+
+
 
 ?>
